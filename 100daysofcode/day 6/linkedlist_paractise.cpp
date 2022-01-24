7e30e8c6416b3206b930da75a4ca4f0053bc7e87
yashrbankar <yashrbankar@gmail.com>
2022-01-24 22:14:04 +0530

    c++ concept

--- 100daysofcode/day 6/linkedlist_paractise.cpp
+++ 100daysofcode/day 6/linkedlist_paractise.cpp
@@ -1,56 +1,220 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
 {
-    public:
+public:
     int data;
-    Node* next;
+    Node *next;
 };
 
 class linkedlist
 {
-    public:
-    Node* root;
+public:
+    int count;
+    Node *first;
     void create();
-    void display(Node *);
-
+    void display();
+    void max();
+    int delete1();
+    bool search();
+    void search_im();
+    void dublicate();
+    void reverse();
+    void reverse_recur(Node* , Node*);
 };
-
-void linkedlist::create()
-{
-    int arr[5]={1,2,3,4,5};
-    Node *temp , *last ;
-    root=new Node;
-    cout<<"enter the root value :: ";
-    cin>>root->data;
-    arr[0]=root->data;
-    root->next=NULL;
-    last=root;
-    for (int i = 1; i < 5; i++)
-    {
-        temp=new Node;
-        temp->data=arr[i];
-        temp->next=NULL;
-        last->next=temp;
-        last=temp;
-    }
-    display(root);
-}
-
-void linkedlist::display(Node * temp)
-{
-    while (temp)
-    {
-        cout<<" "<<temp->data;
-        temp=temp->next;
-    }
-    
-}
 
 int main()
 {
     linkedlist obj;
     obj.create();
-   return 0;
+    // obj.max();
+    // obj.delete1();
+    // cout<<"search result :: "<<obj.search();
+    // obj.search_im();
+    // obj.dublicate();
+    obj.reverse_recur(obj.first , NULL);
+    obj.display();
+    return 0;
+}
+
+void linkedlist::create()
+{
+    int n=7;
+    int arr[n] = {2, 2, 3, 3, 3 , 4 ,4};
+    count=n;
+    Node *temp, *last;
+    first = new Node;
+    cout << "\n enter the first value :: ";
+    cin >> first->data;
+    arr[0] = first->data;
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        temp = new Node;
+        temp->data = arr[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+    // display();
+}
+
+void linkedlist::display()
+{
+    Node *temp = first;
+    while (temp)
+    {
+        cout << " " << temp->data;
+        temp = temp->next;
+    }
+}
+
+void linkedlist::max()
+{
+    int max = -10000, min = 100000;
+    Node *temp = first;
+    while (temp)
+    {
+        if (temp->data > max)
+        {
+            max = temp->data;
+        }
+        if (temp->data < min)
+        {
+            min = temp->data;
+        }
+        temp = temp->next;
+    }
+    cout << "\n max and min :: " << max << " & " << min << endl;
+}
+
+int linkedlist::delete1()
+{
+    display();
+    int pos;
+    cout << "\n Enter the valid position :: ";
+    cin >> pos;
+    Node *p, *q;
+    if (pos <= count)
+    {
+        if (pos == 1)
+        {
+            p = first;
+            first = first->next;
+            delete p;
+            p = NULL;
+        }
+        else
+        {
+            p = first;
+            q = NULL;
+            for (int i = 1; i < pos; i++)
+            {
+                q = p;
+                p = p->next;
+            }
+            q->next = p->next;
+            delete p;
+            p = NULL;
+        }
+    }
+    display();
+}
+
+bool linkedlist::search()
+{
+    display();
+    Node *p = first;
+    int key, x = -1;
+    cout << "enter the key to be search :: ";
+    cin >> key;
+    while (p)
+    {
+        if (p->data == key)
+        {
+            cout << "key is found \n";
+            return true;
+        }
+        else
+        {
+            p = p->next;
+        }
+    }
+    return false;
+}
+
+void linkedlist::search_im()
+{
+    display();
+    int key;
+    cout << " \n enter the key to be search :: ";
+    cin >> key;
+    Node *p = first;
+    Node *q = NULL;
+    while (p)
+    {
+        if (key == p->data)
+        {
+            cout << "key found \n";
+            q->next = p->next;
+            p->next = first;
+            first = p;
+        }
+        q = p;
+        p = p->next;
+    }
+    display();
+}
+
+void linkedlist::dublicate()
+{
+    Node *p = first, *q = first->next;
+    while (q)
+    {
+        if (p->data == q->data)
+        {
+            p->next = q->next;
+            delete q;
+            q = NULL;
+            q=p->next;
+        }
+        else
+        {
+            p = q;
+            q = q->next;
+        }
+    }
+    display();
+}
+
+void linkedlist::reverse()
+{
+    Node *r ,*p, *q;
+    r=q=NULL;
+    p=first;
+    while (p)
+    {
+        r=q;
+        q=p;
+        p=p->next;
+        q->next=r;
+    }
+    first=q;
+    display();
+}
+
+void linkedlist::reverse_recur(Node* p , Node *q)
+{
+    if (p)
+    {
+        reverse_recur( p->next , p );
+        p->next=q;
+    }
+    else
+    {
+        first =q;
+    }
+    
 }
