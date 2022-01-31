2dac1bb78eb59c2c58257aa390846d891e7d1985
yashrbankar <yashrbankar@gmail.com>
2022-01-31 13:25:26 +0530

    changes

--- Abdul Bari Sir  DSA/ADT ARRAY/missing_hashing.cpp
+++ Abdul Bari Sir  DSA/ADT ARRAY/missing_hashing.cpp
@@ -4,26 +4,25 @@ using namespace std;
 class miss
 {
     int A[10] = {3, 7, 4, 9, 12, 6, 1, 11, 2, 10};
-    int l = 1, h = 12;
-    int H[12];
+    int l = 1, h = 12, *H;
 
 public:
     void func();
+    miss()
+    {
+        H = new int[h];
+    }
+
 };
 
-void miss::func()
+void
+miss::func()
 {
-    for (int i = 0; i <12; i++)
-    { 
-        H[i]=0;
-        
-    }
-    
     for (int i = 0; i < 10; i++)
     {
         H[A[i]]++;
     }
-    for (int i = l; i < h; i++)
+    for (int i = l - 1; i < h; i++)
     {
         if (H[i] == 0)
         {
@@ -36,5 +35,6 @@ int main()
 {
     miss obj;
     obj.func();
+
     return 0;
 }
