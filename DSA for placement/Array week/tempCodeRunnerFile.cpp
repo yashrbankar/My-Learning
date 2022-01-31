cb61cc0e9567f21e12abce86b34edf3d38318733
yashrbankar <yashrbankar@gmail.com>
2022-01-31 23:30:13 +0530

    31 jan

---
+++ DSA for placement/Array week/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+obj.insert();
+    // obj.append();
--- other/demo.cpp
+++ other/demo.cpp
@@ -1,145 +1,93 @@
 #include <iostream>
 using namespace std;
 
-class Node
+class Array
 {
 public:
-    Node *lchild;
-    Node *rchild;
-    int data;
-};
-class Queue
-{
-public:
-    int front = -1;
-    int rare = -1;
-    Node *arrq[50];
-    int s = 50;
-    Node *dequeue();
-    void enqueue(Node *);
-};
+    int n;
+    int size = 10;
+    int *H;
+    int length;
+    int *A;
 
-class Tree : public Queue, public Node
-{
-public:
-    Node *t;
-    void create();
-    void preorder(Node *);
-    void Inorder(Node *);
-    void postorder(Node *);
-    int count(Node *);
+    void display();
+    void append();
+    void insert();
+    int search();
+    Array()
+    {
+        A = new int[size];
+
+        cout << "Enter number of elements to be inserted in array :" << endl;
+        cin >> n;
+        length = n;
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Enter value of " << i << " index :" << endl;
+            cin >> A[i];
+        }
+    }
 };
+void Array::display()
+{
 
-void Queue::enqueue(Node *data)
-{
-    if (rare == s - 1)
+    for (int i = 0; i < length; i++)
     {
-        cout << "Queue is full" << endl;
-    }
-    else
-    {
-        rare++;
-        arrq[rare] = data;
+        cout << A[i] << " ";
     }
 }
-Node *Queue::dequeue()
+void Array::append()
 {
-    if (front == rare)
-    {
-        cout << "Queue is empty" << endl;
-    }
-    else
-    {
-        front++;
-        return arrq[front];
-    }
-}
-void Tree::preorder(Node *t)
-{
-    if (t != NULL)
-    {
-        cout << t->data << endl;
-        preorder(t->lchild);
-        preorder(t->rchild);
-    }
-}
-void Tree::Inorder(Node *t)
-{
-    if (t != NULL)
-    {
-        Inorder(t->lchild);
-        cout << t->data << endl;
-        ;
-        Inorder(t->rchild);
-    }
-}
-void Tree::postorder(Node *t)
-{
-    if (t != NULL)
-    {
-        Inorder(t->rchild);
-        Inorder(t->lchild);
-        cout << t->data << endl;
-    }
-}
-
-void Tree::create()
-{
-    Node *root, *t, *p;
     int x;
-    root = new Node;
-    cout << "Enter value of root" << endl;
+    cout << "\nEnter number to be added in array :" << endl;
     cin >> x;
-    root->data = x;
-    root->lchild = root->rchild = NULL;
-    enqueue(root);
-    while (front != rare)
-    {
-        p = dequeue();
-        cout << "Enter data of left child of " << p->data << endl;
-        cin >> x;
-        if (x != -1)
-        {
-            t = new Node;
-            t->data = x;
-            t->lchild = t->rchild = NULL;
-            p->lchild = t;
-            enqueue(t);
-        }
-        cout << "Enter data of left right of " << p->data << endl;
-        cin >> x;
-        if (x != -1)
-        {
-            t = new Node;
-            t->data = x;
-            t->lchild = t->rchild = NULL;
-            p->rchild = t;
-            enqueue(t);
-        }
-    }
-    cout << "......Preorder......." << endl;
-    preorder(root);
-    cout << "......Inorder......." << endl;
-    Inorder(root);
-    cout << "......postorder......." << endl;
-    postorder(root);
-    cout<<"Total number of nodes are "<<count(root);
-
+    A[length] = x;
+    length++;
+    cout << "New Array is : " << endl;
+    display();
 }
-int Tree::count(Node *t)
+void Array::insert()
 {
-    if (t != NULL)
+    int index, x;
+    cout << "\nEnter index at which value is to be insertd " << endl;
+    cin >> index;
+    cout << "Enter value to be inserted : " << endl;
+    cin >> x;
+    for (int i = length; i >= index; i--)
     {
-        int x,y;
-        x = count(t->lchild);
-        y = count(t->rchild);
-        return x + y + 1;
+        A[i + 1] = A[i];
     }
+    A[index] = x;
+    length++;
+    display();
 }
-
+int Array::search()
+{
+    int key;
+    cout << "\nEnter element to be found : " << endl;
+    cin >> key;
+    for (int i = 0; i < length; i++)
+    {
+        if (key == A[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int main()
 {
-    Tree q;
-    q.create();
+    Array a;
+    a.append();
+    a.insert();
+    int ans=a.search();
+    if (ans != 1)
+    {
+        cout << "Element found at index " << ans << endl;
+    }
+    else
+    {
+        cout << "Element not found " << endl;
+    }
     return 0;
 }
