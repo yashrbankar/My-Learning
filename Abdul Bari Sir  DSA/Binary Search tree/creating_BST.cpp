f67d41e983ad57f330d48f79409f90889778144c
yashrbankar <yashrbankar@gmail.com>
2022-02-01 22:35:54 +0530

    BST

--- Abdul Bari Sir  DSA/Binary Search tree/creating_BST.cpp
+++ Abdul Bari Sir  DSA/Binary Search tree/creating_BST.cpp
@@ -7,7 +7,7 @@ public:
     int data;
     Node *lchild;
     Node *rchild;
-};
+} *root = NULL;
 
 Node *insert(Node *p, int key)
 {
@@ -27,7 +27,50 @@ Node *insert(Node *p, int key)
     {
         p->rchild = insert(p->rchild, key);
     }
-    // return t;
+    return t;
+}
+
+void insert_i(int key)
+{
+    Node *r = root, *t, *p = NULL;
+    if (root == NULL)
+    {
+        root = new Node;
+        root->data = key;
+        root->lchild = root->rchild = NULL;
+    }
+    else
+    {
+        while (r)
+        {
+            if (key == r->data)
+            {
+                return;
+            }
+            else if (key > r->data)
+            {
+                p = r;
+                r = r->rchild;
+            }
+            else if (key < r->data)
+            {
+                p = r;
+                r = r->lchild;
+            }
+        }
+
+        t = new Node;
+        t->data = key;
+        t->lchild = t->rchild = NULL;
+        if (p->data < key)
+        {
+            p->rchild = t;
+        }
+        else if (p->data > key)
+        {
+            p->lchild = t;
+        }
+    }
 }
 
 void inorder(Node *t)
@@ -42,10 +85,13 @@ void inorder(Node *t)
 
 int main()
 {
-    Node *root = NULL;
-    root = insert(root, 30);
-    insert(root, 20);
-    insert(root, 50);
+
+    // root = insert(root, 10); // recursive version
+    insert_i(40);
+    insert_i(20);
+    insert_i(40);
+    insert_i(50);
+    insert_i(60);
     inorder(root);
     return 0;
 }
