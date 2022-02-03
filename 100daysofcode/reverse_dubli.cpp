9244635e14a2318f320cfa0f50e764b2da9a7f32
yashrbankar <yashrbankar@gmail.com>
2022-02-03 07:46:24 +0530

    reversing the linkedlist my method

---
+++ 100daysofcode/reverse_dubli.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+  public:
+  Node* prev;
+  int data;
+  Node* next;
+};
+
+
+class Linkedlist
+{
+    public:
+    Node* first;
+    void create1();
+    void display();
+    void reverse();
+};
+
+void Linkedlist::create1()
+{
+    int arr[]={2,4,6,8,9};
+    Node* temp ,*last;
+    first=new Node;
+    first->data=arr[0];
+    first->next=first->prev=NULL;
+    last=first;
+    for (int i = 1; i < 5; i++)
+    {
+        temp=new Node;
+        temp->data=arr[i];
+        temp->next=temp->next=NULL;
+        last->next=temp;
+        temp->prev=last;
+        last=temp;
+    }
+    temp->next=first;
+    first->prev=temp;  
+}
+
+void Linkedlist::display()
+{
+    Node *t=first;
+    do
+    {
+        cout<<" "<<t->data;
+        t=t->next;
+    } while (t!=first);
+
+    //  test linkedlist ;
+    // while (t)
+    // {
+    //     cout<<" "<<t->data;
+    //     t=t->next;
+    // }  
+}
+
+void Linkedlist::reverse()
+{
+    Node* pt=first;
+    Node* t1,*t2;
+
+    do
+    {
+
+        t1=pt->next;
+        t2=pt->prev;
+        Node* temp=t1;
+        t1=t2;
+        t2=temp;
+        pt=t2;
+    } while (pt!=first);
+    first=pt;
+    
+    display();
+}
+
+
+
+int main()
+{
+    Linkedlist obj;
+    obj.create1();
+    obj.display();
+    cout<<"\n";
+    obj.reverse();
+   
+   return 0;
+}
