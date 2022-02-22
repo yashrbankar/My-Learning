89e1f8e9fdbddc551ccda4beee8703cb1904c8aa
yashrbankar <yashrbankar@gmail.com>
2022-02-23 01:33:06 +0530

    testing

--- Recursion/recur/recur_func_natural.cpp
+++ Recursion/recur/recur_func_natural.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 int nature(int a)
 {
-  if (a!=1)
+  if (a>0)
   {
+      cout<<" "<<a;
       nature(a-1);
-      
+      cout<<" "<<a;
   }
-  
-  cout<<a<<endl;
   
 }
 
@@ -18,7 +17,7 @@ int main()
  int n;
  cout<<"Enter n";
  cin>>n;
- nature(n);
+ nature(5);
 
 return 0;
 }
