9076d3b962eabc826b02faf874b08870ab538b01
yashrbankar <yashrbankar@gmail.com>
2022-03-31 22:59:01 +0530

    opeation

---
+++ java1/Operation.java
@@ -0,0 +1,14 @@
+class Operation
+{
+   public static void main(String args[])
+   {
+       int a=14,b=5;
+       int c=a/b;
+       float d=(float)a/b;// type casting 
+       System.out.println("integer :"+c);
+       System.out.println("float :"+d);
+   }
+}
+
+       
+       
