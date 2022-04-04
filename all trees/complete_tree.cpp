698d01f117180479a07895f67bbc35a94c4a057f
yashrbankar <yashrbankar@gmail.com>
2022-04-04 21:51:41 +0530

    commit

--- all trees/complete_tree.cpp
+++ all trees/complete_tree.cpp
@@ -80,6 +80,9 @@ class Tree:public Node,public Stack,public Queue
     Node* root;
     void create_level();
     void preorder(Node*);
+    int height(Node*);
+    int diameter(Node*);
+    void levelorder();
 };
 
 void Tree::create_level()
@@ -109,7 +112,7 @@ void Tree::create_level()
            enQueue(temp);
         }
 
-        cout<<"enter the data to be stored left side of :: "<<q->data<<" : ";
+        cout<<"enter the data to be stored right side of :: "<<q->data<<" : ";
         cin>>d;
         if(d!=-1)
         {
@@ -132,11 +135,49 @@ void Tree::preorder(Node* root)
     }
 }
 
+int Tree::height(Node* root)
+{
+    if(root)
+    {
+       int a=height(root->lchild);
+       int b=height(root->rchild);
+       return max(a,b)+1;
+    }
+    return 0; // 
+}
+
+int Tree::diameter(Node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    int cur;
+    cur=root->data;
+    
+    int a=diameter(root->lchild);
+    cur=root->data;//current
+    int b=diameter(root->rchild);
+    cur=root->data;
+    int c=height(root->lchild)+height(root->rchild);
+    cur=root->data;
+    int ans=max(a,max(b,c));
+    return ans;
+}
+
+
+
 int main()
 {
     Tree obj;
+    // 1 2 3 4 5 -1 -1 6 -1 -1 7 8 -1 -1 9 -1 -1 -1 -1
     obj.create_level();
     obj.preorder(obj.root);
+    cout<<"Height is :: "<<obj.height(obj.root);
+    cout<<"diameter is :: "<<obj.diameter(obj.root);
+    cout<<"\n";
+    obj.preorder(obj.root);
+
    
    return 0;
 }
---
+++ java1/hackathon.java~
@@ -0,0 +1,11 @@
+import java.util.*;
+class input{
+    
+    public static void main(String sid[]){
+        Scanner yash=new Scanner(System.in);
+        String dd;
+        System.out.println("Enter your name ::");
+        dd=yash.nextLine();
+        System.out.println(dd);
+    }
+}
