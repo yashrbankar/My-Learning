be828c4e531f3035fd6e236f744e73e6d2e34b9d
yashrbankar <yashrbankar@gmail.com>
2022-04-05 00:11:32 +0530

    changes

---
+++ java1/IDoperation.java
@@ -0,0 +1,14 @@
+class IDoperation
+{
+   public static void main(String args[])
+   {
+     System.out.println("Increament & Decrement");
+      int x=5,y=10,z;
+      z=2*x++ +2* ++y;
+      System.out.println("Increament & Decrement"+ z);
+      
+     
+     
+   }
+ }
+ 
