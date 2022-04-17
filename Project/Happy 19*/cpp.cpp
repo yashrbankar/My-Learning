51a71130074f98724d95e02ff55dd8644acf1392
yashrbankar <yashrbankar@gmail.com>
2022-04-18 00:22:21 +0530

    changed

--- Project/Happy 19*/cpp.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-   cout<<"Happy 19*";
-   return 0;
-}
--- Project/Happy 19*/java.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-class Happy
-{
-    public static void main(String Yash[])
-    {
-           System.out.println("Happy 19*");
-    }
-}
