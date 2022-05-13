aa74d575947821229de7f81d8b6de11289444beb
yashrbankar <yashrbankar@gmail.com>
2022-05-13 22:39:21 +0530

    rotation of the array using the juggling algo

--- Data Structures/Array/rotate.cpp
+++ Data Structures/Array/rotate.cpp
@@ -1,60 +1,54 @@
-// C++ program to rotate an array by
-// d elements
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-/*Function to get gcd of a and b*/
-int gcd(int ro, int si)
+int gcd(int ro , int si)
 {
-	if (ro == 0)
-		return si;
-
-	else
-		return gcd(si, ro% si);
-}
-
-/*Function to left rotate arr[] of siz n by d*/
-void leftRotate(int arr[], int d, int n)
-{
-	/* To handle if d >= n */
-	d = d % n;
-	int g_c_d = gcd(d, n);
-	for (int i = 0; i < g_c_d; i++) {
-		/* move i-th values of blocks */
-		int temp = arr[i];
-		int j = i;
-
-		while (1) {
-			int k = j + d;
-			if (k >= n)
-				k = k - n;
-
-			if (k == i)
-				break;
-
-			arr[j] = arr[k];
-			j = k;
-		}
-		arr[j] = temp;
+	if(si==0)
+	{
+       return ro;
 	}
-}
+	else
+	{
+		return gcd(ro,ro%si);
+	}
 
-// Function to print an array
-void printArray(int arr[], int size)
+}
+void rotate(int arr[] , int d , int n)
 {
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-}
+	d=d%n;
+   int g_c_d=gcd(d , n);
 
-/* Driver program to test above functions */
+   for (int i = 0; i < g_c_d; i++)
+   {
+	   int temp=arr[i];
+	   int j=i;
+	   while (1)
+	   {
+		   int k=j+d;
+		   if(k>=n)
+		   {
+			   k=k-n;
+		   }
+		   if(k==i)
+		   {
+             break;
+		   }
+		   arr[j]=arr[k];
+		   j=k;
+	   }
+	   arr[j]=temp;
+   }
+}
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-	int n = sizeof(arr) / sizeof(arr[0]);
 
-	// Function calling
-	leftRotate(arr, 2, n);
-	printArray(arr, n);
-
-	return 0;
+	int arr[]={1,2,3,4,5,6,7};
+	int n=sizeof(arr)/sizeof(int);
+    
+	rotate(arr , 7, n);
+	for (int i = 0; i <n; i++)
+	{
+		cout<<" "<<arr[i];
+	}
+   return 0;
 }
