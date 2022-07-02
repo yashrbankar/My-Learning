4d4c21dff6998672ba9bfd4a0120353cc783897e
yashrbankar <yashrbankar@gmail.com>
2022-07-02 22:43:17 +0530

    hashing

--- Data Structures/Array/complete_array.cpp
+++ Data Structures/Array/complete_array.cpp
@@ -1,48 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void missing(int arr[], int n)
-{
-    sort(arr,arr+5);
-    int i=0;
-    int diff =arr[0]-i;
-    for(i=0;i<n;i++)
-    {
-        if(diff!=arr[i]-i)
-        {
-            cout<<"\n missing element ::"<<diff+i;
-            diff++;
-        }
-    }
-}
-
-void multipleMissing(int arr[], int n)
-{
-    // sort(arr,arr+5);
-    int i=0,diff;
-    diff=arr[i]-i;
-    for ( i = 1; i < n; i++)
-    {
-        if(diff!=arr[i]-i)
-        {
-            int tempDiff=arr[i]-i-diff;
-            while (tempDiff>0)
-            {
-                cout<<"\n missing Element is :"<<diff+i;
-                diff++;
-                tempDiff--;
-            }
-        }
-    }
-    
-}
-
 
 
 int main()
 {
-    int arr[]={11,13,15,20,25};
-    //missing(arr,5);
-    multipleMissing(arr,5);
+    int arr[]={1,2,2,2,3,3,3,4};
+    int H[10]={0,0,0,0,0,0,0,0,0,0};
+
+    for (int i = 0; i < 7; i++)
+    {
+        H[arr[i]]++;
+    }
+
+    for(int i=1;i<10;i++)
+    {
+        if(H[i]>0)
+        {
+            cout<<H[i]<< " times "<<i<<endl;
+        }
+    }
+
+
+    
    return 0;
 }
