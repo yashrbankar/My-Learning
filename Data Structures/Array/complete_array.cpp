5c296783407f5cd57bc60625d9b73ec7275ae4a0
yashrbankar <yashrbankar@gmail.com>
2022-07-02 15:16:13 +0530

    missing

--- Data Structures/Array/complete_array.cpp
+++ Data Structures/Array/complete_array.cpp
@@ -16,11 +16,33 @@ void missing(int arr[], int n)
     }
 }
 
+void multipleMissing(int arr[], int n)
+{
+    // sort(arr,arr+5);
+    int i=0,diff;
+    diff=arr[i]-i;
+    for ( i = 1; i < n; i++)
+    {
+        if(diff!=arr[i]-i)
+        {
+            int tempDiff=arr[i]-i-diff;
+            while (tempDiff>0)
+            {
+                cout<<"\n missing Element is :"<<diff+i;
+                diff++;
+                tempDiff--;
+            }
+        }
+    }
+    
+}
+
 
 
 int main()
 {
-    int arr[]={17,12,13,15,11};
-    missing(arr,5);
+    int arr[]={11,13,15,20,25};
+    //missing(arr,5);
+    multipleMissing(arr,5);
    return 0;
 }
