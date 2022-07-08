e6bdfc24200e274c7433d0fa203a76feddf8dd3a
yashrbankar <yashrbankar@gmail.com>
2022-07-08 16:13:25 +0530

    changes

---
+++ Paractice/string.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+// ALL in one String STL Function
+int main()
+{
+    //string str ="This the yash Bankar";
+    // Printing the 
+   // cout<<str;
+
+/////////////////////////////////////////////
+
+    // input string
+    /*
+     cout<<"String input 1 ::";
+     cin>>str; // int take single word as input
+     cout<<"Hello "<<str;
+    */
+//////////////////////////////////////////////
+
+     // getline
+     /*
+    cout<<"String input 2 ::";
+    getline(cin,str); // use instead of cin<<
+    cout<<"Hello "<<str;
+    */
+////////////////////////////////////////
+
+     // swap of to string
+     /*
+     string a="yash",b="satyam";
+     cout<<a<<" "<<b<<endl;
+     a.swap(b);
+     cout<<a<<" "<<b<<endl;
+     */
+ /////////////////////////////////////////////////   
+
+    // Find :: Find sub string in string
+    /*
+    string main="This is yash" ,sub="yash";
+    cout<<"starting index  is : "<<main.find(sub);
+    cout<<"\n starting character is :: "<<main[main.find(sub)];
+    */
+//////////////////////////////////////////////
+
+   // Replace in string
+   /*
+     string str ="I love you yashb";
+     str.replace(10,5,"");
+     cout<<str;
+   */
+//////////////////////////////////////
+
+  // Sub string 
+  /*
+      string str="I love you yash";
+      cout<<str.substr(7,3);
+  */
+
+/////////////////////////////////
+
+   // erase
+   /*
+    string str("yash bankar");
+    cout<<str.erase(4 , 3);
+   */
+///////////////////////////////
+
+string str
+
+cout<<"Thank you!";
+   return 0;
+}
