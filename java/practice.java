7c1f9358372b7a47c8d8f5d8f2da3402f9fa1918
yashrbankar <yashrbankar@gmail.com>
2022-09-18 22:54:27 +0530

    java practice

---
+++ java/practice.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class practice
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("yash");
+        double a=10;
+        int b=20;
+        int sum=(int)a+b;
+        float x=100.111f;
+        a=x;
+        
+        
+        String str=sc.next();
+        System.out.println("hello "+a);
+        System.out.println(sum);
+    }
+};
